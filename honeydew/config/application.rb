528feea7d2dbca8e1c324d9c5382f63fe26f715e
Adam Baca <baca.adam@me.com>
2015-03-06 08:26:10 -0700

    updated some precompile stuff for heroku

--- honeydew/config/application.rb
+++ honeydew/config/application.rb
@@ -21,5 +21,6 @@ module Riorunner
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     config.assets.paths << Rails.root.join("vendor", "assets")
+    config.assets.initialize_on_precompile = false
   end
 end
