214d9a0c3a90b25f5b32550d52205eba78835e74
Adam Baca <baca.adam@me.com>
2015-03-06 12:38:36 -0700

    finshed landing page and modified app for beta

--- honeydew/config/environments/production.rb
+++ honeydew/config/environments/production.rb
@@ -88,4 +88,21 @@ Rails.application.configure do
           :secret_access_key => ENV['0u/eJNMhtkQ1J0SR1IH3U/qsWg03mjH8VKWmdxSP']
       }
   }
+
+  config.action_mailer.default_url_options = { :host => 'gohoneydewbeta.herokuapp.com' }
+
+
+  #These settings are for the sending out email for active admin and consequently the   devise mailer
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings =
+      {
+
+          :address            => 'smtp.gmail.com',
+          :port               => 587,
+          :domain             => 'gmail.com', #you can also use google.com
+          :authentication     => :plain,
+          :user_name          => 'gohoneydewbeta@gmail.com',
+          :password           => 'fucel-8L'
 end
